df65c5326108c1eefad4564554e0e1aa73313b3f
PvelozCH <pveloz.ch@gmail.com>
2025-12-01 10:12:10 -0300

    Add files via upload
    Se lee un reporte Unifier y se crea base de datos sqlite3 con esa información. Falta verificar con una consulta si efectivamente se guardaron bien los datos...

--- Main.py
+++ Main.py
@@ -1,11 +1,18 @@
 from Service.UnifierService import uniController
+import traceback,sys
 
 def operaUnifierUDR():
-    print("Se cargara reporte Unifier")
-    token = uniController.getTokenUnifier()
-    nomReporte = input("Ingresa el nombre del reporte : ")
-    jsonUnifier = uniController.loadReportUnifier(token,nomReporte)
-    return jsonUnifier
+    try:
+
+        print("Se cargara reporte Unifier")
+        token = uniController.getTokenUnifier()
+        print("Se cargará y creará base de datos sql a partir de reporte UDR")
+        nomReporte = input("Ingresa el nombre del reporte : ")
+        a,b,c = uniController.loadReportUnifierV2(token,nomReporte)
+        uniController.creacionDbUnifier(a,b,c)
+    except Exception as e:
+        print("Error completo : ",traceback.format_exc())
+        print("Error : ",e)
 
 def main():
     print("### Ejecución del programa ###")
--- Service/UnifierService.py
+++ Service/UnifierService.py
@@ -1,3 +1,4 @@
+import traceback,sys
 import sqlite3
 import requests
 import os
@@ -9,114 +10,143 @@ class uniController:
     def getTokenUnifier():
         token = ""
         data = ""
-        url = os.getenv("tokenUrlUNIFIER")
-        username = os.getenv("UserUNIFIER")
-        password = os.getenv("PasswordUNIFIER")
+        try:
 
-        response = requests.get(url, auth=(username,password))
+            url = os.getenv("tokenUrlUNIFIER")
+            username = os.getenv("UserUNIFIER")
+            password = os.getenv("PasswordUNIFIER")
 
-        if response.status_code == 200:
-            data = response.json()
-            token = data['token']
-        else:
-            print("ERROR",response.status_code,response.text)
-        
+            response = requests.get(url, auth=(username,password))
+
+            if response.status_code == 200:
+                data = response.json()
+                token = data['token']
+            else:
+                print("ERROR",response.status_code,response.text)
+        except Exception as e:
+            print("Error completo : ",traceback.format_exc())
+            print("Error con Token : ",e)
         return token
     # Carga reporte y solamente trae json completo sin formatear (clásico)
     def loadReportUnifier(token,nomReporte):
         data =""
         report_header =""
         report_row = ""
-        url = os.getenv("udrUrlUNIFIER")
+        try:
 
-        headers = {
-            "Authorization": f"Bearer {token}",
-            "Content-Type": "application/json"
-        }
-        payload = {
-            "reportname":nomReporte
-        }
-        response = requests.post(url,json=payload,headers=headers)
+            url = os.getenv("udrUrlUNIFIER")
 
-        # Para enviar a 
-        if response.status_code == 200:
-            # Json que contiene la data completa
-            data = response.json()
-            # Json que contiene solo las cabeceras
-            report_header = response.json()['data'][0]['report_header']
-            # Json que contiene los datosdel reporte
-            report_row = response.json()['data'][0]['report_row']
-        else:
-            print("ERROR",response.status_code,response.text)
-        
+            headers = {
+                "Authorization": f"Bearer {token}",
+                "Content-Type": "application/json"
+            }
+            payload = {
+                "reportname":nomReporte
+            }
+            response = requests.post(url,json=payload,headers=headers)
+
+            # Para enviar a 
+            if response.status_code == 200:
+                # Json que contiene la data completa
+                data = response.json()
+                # Json que contiene solo las cabeceras
+                report_header = response.json()['data'][0]['report_header']
+                # Json que contiene los datosdel reporte
+                report_row = response.json()['data'][0]['report_row']
+            else:
+                print("ERROR",response.status_code,response.text)
+        except Exception as e:
+            print("Error completo : ",traceback.format_exc())
+            print("Error reporte Unifier: ",e)
         return data
 
     # Carga de reporte normalizado, extraerá cabeceras y campos de manera ordenada lista para ingresar en tabla sqlite
     def loadReportUnifierV2(token,nomReporte):
+        try:
 
-        """
-        - Consume el reporte de Unifier (JSON)
-        - Normaliza el JSON usando los nombres reales del header
-        - Crea una tabla SQLite con nombres correctos
-        - Inserta las filas del reporte
-        """
-        nom_tabla = nomReporte.replace(" ","_").replace("-","_")
-        normalized_rows = []
-        data =""
-        report_header =""
-        report_row = ""
-        url = os.getenv("udrUrlUNIFIER")
+            """
+            - Consume el reporte de Unifier (JSON)
+            - Normaliza el JSON usando los nombres reales del header
+            - Crea una tabla SQLite con nombres correctos
+            - Inserta las filas del reporte
+            """
+            nom_tabla = nomReporte.replace(" ","_").replace("-","_")
+            normalized_rows = []
+            data =""
+            report_header =""
+            report_row = ""
+            url = os.getenv("udrUrlUNIFIER")
 
-        headers = {
-            "Authorization": f"Bearer {token}",
-            "Content-Type": "application/json"
-        }
-        payload = {
-            "reportname":nomReporte
-        }
-        response = requests.post(url,json=payload,headers=headers)
+            headers = {
+                "Authorization": f"Bearer {token}",
+                "Content-Type": "application/json"
+            }
+            payload = {
+                "reportname":nomReporte
+            }
+            response = requests.post(url,json=payload,headers=headers)
 
-        # Para enviar a 
-        if response.status_code == 200:
-            # Json que contiene la data completa
-            data = response.json()
+            # Para enviar a 
+            if response.status_code == 200:
+                # Json que contiene la data completa
+                data = response.json()
 
-            # Json que contiene solo las cabeceras
-            report_header = response.json()['data'][0]['report_header']
-            column_map = {col : report_header[col]["name"] for col in headers} # lista de los campos que vienen como headers
+                # Json que contiene solo las cabeceras
+                report_header = response.json()['data'][0]['report_header']
+                column_map = {col : report_header[col]["name"] for col in report_header} # lista de los campos que vienen como headers
 
-            # Json que contiene los datosdel reporte
-            report_row = response.json()['data'][0]['report_row']
+                # Json que contiene los datosdel reporte
+                report_row = response.json()['data'][0]['report_row']
 
-            # Cada row se agrega a lista "normalizada"
-            for row in data["data"][0]["report_row"]:
-                new_row = {}
-                for col, value in row.items():
-                    new_row[column_map[col]] = value
-                normalized_rows.append(new_row)
-        else:
-            print("===========")
-            print("###ERROR :: ",response.status_code,response.text," ###")
-            print("===========")
+                # Cada row se agrega a lista "normalizada"
+                for row in data["data"][0]["report_row"]:
+                    new_row = {}
+                    for col, value in row.items():
+                        new_row[column_map[col]] = value
+                    normalized_rows.append(new_row)
+            else:
+                print("===========")
+                print("###ERROR :: ",response.status_code,response.text," ###")
+                print("===========")
+        except Exception as e:
+            print("Error completo : ",traceback.format_exc())
+            print("Error reporte Unifier : ",e)
 
         return column_map,normalized_rows,nom_tabla
     
     def creacionDbUnifier(column_map,normalized_rows,nom_tabla):
-        # Conexión
-        conn = sqlite3.connect("UnifierDB.db")
-        cur = conn.cursor()
+        try:
+            # Conexión
+            nomDB ="UnifierDB.db"
+            conn = sqlite3.connect(nomDB)
+            cur = conn.cursor()
+            
+            # Toma columnas para automatizar creación de tabla 
+            columnas = list(normalized_rows[0].keys())
+            ddl = ", ".join([f'"{col}" TEXT' for col in columnas])
+
+            # FALTA CREAR TABLA O NO HACERLO EN CASO DE QUE YA EXISTA!!!!!!
+            cur.execute(f"DROP TABLE IF EXISTS {nom_tabla}")
+            cur.execute(f"CREATE TABLE {nom_tabla}({ddl})")
+
+            registros = ", ".join(["?"] * len(columnas))
+
+            for r in normalized_rows:
+                cur.execute(f'INSERT INTO "{nom_tabla}" VALUES ({registros})',list(r.values()))
+            
+            print(f"Tabla '{nom_tabla}' creada con exito con {len(normalized_rows)} filas en BDD {nomDB}")
+
+            #print(cur.execute(f"SELECT * FROM {nom_tabla}"))
+
+            conn.commit()
+            conn.close()
+
+
         
-        # Toma columnas para automatizar creación de tabla 
-        columnas = list(normalized_rows[0].keys())
-        ddl = ", ".join([f'"{col}" TEXT' for col in columns])
-
-        # FALTA CREAR TABLA O NO HACERLO EN CASO DE QUE YA EXISTA!!!!!!
-        cur.execute(f"CREATE TABLE {nom_tabla}({ddl})")
-
-
-
-
-
+        except Exception as e:
+            print("Error completo : ",traceback.format_exc())
+            print("Error : ",e)
+        
     def fetchBPrecordList(token):
         data =""
         bpname = os.getenv("nomBpnameUNIFIER")
